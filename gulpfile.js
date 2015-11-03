0139770064809b8eaf2f1bae78fce31e366d3bfc
Pompeu <itacir@hotmail.com>
2015-11-02 22:56:08 -0200

    npm build prod with gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'); 
+const gulp = require('gulp'),
+      uglify = require('gulp-uglify'),
+      concat = require('gulp-concat'); 
 
-gulp.task('js', function() {
-  return gulp.src('src/js/lib/*.js')
+gulp.task('js-prod', () => {
+  return  gulp.src('src/js/lib/*.js')
     .pipe(concat('simpleajax.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/js/'));     
 });
 
-gulp.task('default',['js']);
+gulp.task('js-cp', () => {
+  return  gulp.src('src/js/lib/*.js')
+    .pipe(concat('simpleajax.js'))
+    .pipe(gulp.dest('dist/js/')); 
+})
+
+gulp.task('default',['js-cp', 'js-prod']);
