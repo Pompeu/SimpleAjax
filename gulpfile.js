314940a4399d5807f12fceb07b801f65504065b5
Pompeu <itacir@hotmail.com>
2015-11-02 22:29:45 -0200

    Promise style in ajax

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('js', function() {
   return gulp.src('src/js/lib/*.js')
     .pipe(concat('simpleajax.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('src/js/lib/'));     
+    .pipe(gulp.dest('dist/js/'));     
 });
 
 gulp.task('default',['js']);
--- src/js/lib/simpleajax.js
+++ src/js/lib/simpleajax.js
@@ -1,37 +1,28 @@
-var $ = (function(){
+var ajaxPromise = (function(){
   'use strict';
+  var req =  new XMLHttpRequest();
 
-  var service = {};
-  
-  service.ajaxservice = function (method,url,callback,data) {
+  return function(method, url, data, contentType) {
+    var req =  new XMLHttpRequest();
+    var data = JSON.stringify(data) || null
 
-    var req = new XMLHttpRequest();
-   
-    req.open(method,url,true);     
+    contentType = contentType || "application/json";
 
-    if(!data){
-      req.send();
-    }else { 
-      req.setRequestHeader("Content-Type", "application/json");
-      req.send(JSON.stringify(data));
-    }
-
-    req.addEventListener("load",success,false);
-    req.addEventListener("error",error , false);
+    if(!url) throw "url is required!"
+    else req.open(method.toUpperCase(),url,true)
+    req.setRequestHeader("Content-Type", contentType);
+    req.send(data);
     
-    function success() {
-      if(req.status < 300 && req.readyState === 4){
-        return callback(null,JSON.parse(req.response));
+    return new Promise( (resolve, reject) => {
+      req.addEventListener("load",success,false);
+      function success() {
+        if(req.status < 300 && req.readyState === 4){
+          return resolve(JSON.parse(req.response))
+        }
+        else if(req.status >= 300){
+          return reject({ response : req.statusText, status : req.status})
+        }
       }
-      else if(req.status >= 300){
-        return callback(new Error("Request failed: " +
-                            req.statusText),null);
-      }
-    };
-
-    function error() {
-      return callback(new Error("Network error"),null);
-    };
-  };
-  return service;
+    });
+  } 
 })();
