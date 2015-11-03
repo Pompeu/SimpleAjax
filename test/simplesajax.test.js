95e59084d7745051cd2e33e78f78c9b3662125e1
Pompeu <itacir@hotmail.com>
2015-11-02 22:29:29 -0200

    add new testes for Promise style

--- test/simplesajax.test.js
+++ test/simplesajax.test.js
@@ -1,36 +1,45 @@
-describe('Simple Ajax lib Test', function() {
+describe('Simple Ajax lib Test',  () => {
 
-  it('should have property ajaxservice', function () {
-    var ajax = $;        
+  it('should have property ajaxservice', () => {
+    var ajax = ajaxPromise;        
     expect(ajax).toBeDefined();
-    expect(ajax.ajaxservice).toBeDefined();
   });
 
-  it('should GET request recive true', function (done) {
-    var ajax = $;
-    var url = "https://bloog-limp.herokuapp.com/api/alunos/";
+  it('should GET request recive data', done => {
+    var ajax = ajaxPromise;
+    var url = "http://redeconhecimentos.herokuapp.com/api/user";
     var method = "GET";
-
-    ajax.ajaxservice(method, url, function(err , data) {
-      expect(err).toBeNull();
-      expect(data).toBeTruthy();
-      data.forEach(function(aluno) {
-        expect(aluno.name).toBeTruthy();
-      })      
-      done();
-    });
-
+    ajax(method,url)
+      .then(function(data) {
+        expect(data).toBeTruthy()
+        done()
+      })
   });
 
-  it('should recive http request', function (done) {
-    var ajax = $;
-    var url = null;
-    var method = "GET";
-    ajax.ajaxservice(method, url, function(err , data) {
-      expect(err).toBeTruthy();
-      expect(data).toBeNull();
-      done();
-    });  
+  it('should Post request with  bad url get same error on promise', done => {
+    var ajax = ajaxPromise;
+    var url = "http://redeconhecimentos.herokuapp.com/1api/user";
+    var method = "POST";
+    var data = {
+        email: "opa@opa.com",
+        name: "Itacir",
+        sname: "Pompeu",
+        password: "12345678"
+    };
+    ajax(method,url,data)
+      .then(data => {
+        expect(data).toBeTruthy()
+      },err => {
+        expect(err).toBeTruthy()
+        done()
+      })
+      
+  });
+
+  it('request should be have an have url', done => {
+    var ajax = ajaxPromise;
+    expect(ajax).toThrow();
+    done();
   });
 
 });
