fdc5d29d824ee89e3a72c1a9b66ea1a7e3f45025
Pompeu <itacir@hotmail.com>
2015-11-07 20:35:49 -0200

    TypeError added and same duplicate code remove

--- src/js/lib/simpleajax.js
+++ src/js/lib/simpleajax.js
@@ -1,15 +1,16 @@
 var ajaxPromise = (function(){
   'use strict';
-  var req =  new XMLHttpRequest();
 
-  return function(method, url, data, contentType) {
-    var req =  new XMLHttpRequest();
-    var data = JSON.stringify(data) || null
+   var req =  new XMLHttpRequest();
+
+  return function ajaxparams(method, url, data, contentType) {
+
+    var data = JSON.stringify(data) || null;
 
     contentType = contentType || "application/json";
 
-    if(!url) throw "url is required!"
-    else req.open(method.toUpperCase(),url,true)
+    if(!url) return function () {throw new TypeError("url is required!");};
+    else req.open(method.toUpperCase(),url,true);
     req.setRequestHeader("Content-Type", contentType);
     req.send(data);
     
@@ -17,12 +18,12 @@ var ajaxPromise = (function(){
       req.addEventListener("load",success,false);
       function success() {
         if(req.status < 300 && req.readyState === 4){
-          return resolve(JSON.parse(req.response))
+          return resolve(JSON.parse(req.response));
         }
         else if(req.status >= 300){
-          return reject({ response : req.statusText, status : req.status})
+          return reject({ response : req.statusText, status : req.status});
         }
       }
     });
-  } 
+  };
 })();
