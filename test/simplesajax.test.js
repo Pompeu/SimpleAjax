778c9aa185b16f25e8195c9ac83c5242961a9d79
Pompeu <itacir@hotmail.com>
2015-11-07 20:37:26 -0200

    code linted, and es6 let added

--- test/simplesajax.test.js
+++ test/simplesajax.test.js
@@ -1,44 +1,49 @@
+'use strict';
+
 describe('Simple Ajax lib Test',  () => {
 
   it('should have property ajaxservice', () => {
-    var ajax = ajaxPromise;        
+    let ajax = ajaxPromise;
     expect(ajax).toBeDefined();
   });
 
   it('should GET request recive data', done => {
-    var ajax = ajaxPromise;
-    var url = "http://redeconhecimentos.herokuapp.com/api/user";
-    var method = "GET";
+    let ajax = ajaxPromise;
+    let url = "http://redeconhecimentos.herokuapp.com/api/user";
+    let method = "GET";
+
     ajax(method,url)
       .then(function(data) {
-        expect(data).toBeTruthy()
-        done()
-      })
+        expect(data).toBeTruthy();
+        done();
+      });
   });
 
   it('should Post request with  bad url get same error on promise', done => {
-    var ajax = ajaxPromise;
-    var url = "http://redeconhecimentos.herokuapp.com/1api/user";
-    var method = "POST";
-    var data = {
-        email: "opa@opa.com",
-        name: "Itacir",
-        sname: "Pompeu",
-        password: "12345678"
-    };
+    let ajax = ajaxPromise,
+        url = "http://redeconhecimentos.herokuapp.com/1api/user",
+        method = "POST",
+          data = {
+          email: "opa@opa.com",
+          name: "Itacir",
+          sname: "Pompeu",
+          password: "12345678"
+        };
+
     ajax(method,url,data)
       .then(data => {
-        expect(data).toBeTruthy()
+        expect(data).toBeTruthy();
       },err => {
-        expect(err).toBeTruthy()
-        done()
-      })
+        expect(err).toBeTruthy();
+        done();
+      });
       
   });
 
   it('request should be have an have url', done => {
-    var ajax = ajaxPromise;
-    expect(ajax).toThrow();
+    let ajax = ajaxPromise;
+
+    expect(ajax()).toThrowError(TypeError, "url is required!");
     done();
   });
 
