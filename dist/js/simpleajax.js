6fe2eb0e64b3c81a8d3e0a660504c358ae43a3a4
pompeu <itacir@hotmail.com>
2016-07-20 12:20:03 -0300

    add babel es 2015 , and rebuild

--- dist/js/simpleajax.js
+++ dist/js/simpleajax.js
@@ -1,27 +1,39 @@
-var ajaxPromise = (function(){
-  'use strict';
+'use strict';
+
+window.ajaxPromise = function () {
+
+  var req = new XMLHttpRequest();
 
   return function ajaxparams(method, url, data, contentType) {
-    var req =  new XMLHttpRequest();
-    var data = JSON.stringify(data) || null;
 
-    contentType = contentType || "application/json";
+    data = JSON.stringify(data) || null;
 
-    if(!url) return function () {throw new TypeError("url is required!");};
-    else req.open(method.toUpperCase(),url,true);
-    req.setRequestHeader("Content-Type", contentType);
+    contentType = contentType || 'application/json';
+
+    if (!url) {
+      return Promise.reject('url is required!');
+    } else {
+      req.open(method.toUpperCase(), url, true);
+    }
+
+    req.setRequestHeader('Content-Type', contentType);
     req.send(data);
-    
-    return new Promise(function(resolve, reject)  {
-      req.addEventListener("load",success,false);
+
+    return new Promise(function (resolve, reject) {
+
+      req.addEventListener('load', success, false);
+
       function success() {
-        if(req.status < 300 && req.readyState === 4){
+
+        if (req.status < 300 && req.readyState === 4) {
           return resolve(JSON.parse(req.response));
-        }
-        else if(req.status >= 300){
-          return reject({ response : req.statusText, status : req.status});
+        } else if (req.status >= 300) {
+          return reject({
+            response: req.statusText,
+            status: req.status
+          });
         }
       }
     });
   };
-})();
+}();
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 'use strict';
 
-const gulp = require('gulp'),
-  uglify = require('gulp-uglify'),
-  concat = require('gulp-concat'); 
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat'); 
+const babel = require('gulp-babel');
 
 gulp.task('js-prod', () => {
   return  gulp.src('src/js/lib/*.js')
+    .pipe(babel())
     .pipe(concat('simpleajax.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js/'));     
+    .pipe(gulp.dest('dist/js/'));
 });
 
 gulp.task('js-cp', () => {
   return  gulp.src('src/js/lib/*.js')
+    .pipe(babel())
     .pipe(concat('simpleajax.js'))
     .pipe(gulp.dest('dist/js/')); 
 });
