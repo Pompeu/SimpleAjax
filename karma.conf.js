ac4307b9d44b43a079234c167d9632c0ba472722
pompeu <itacir@hotmail.com>
2016-07-20 11:54:57 -0300

    repair karma.conf to detect all js in src

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'test/**/*.test.js',
-      'src/js/**/*.js'
+      'src/js/lib/**/*.js'
     ],
 
 
